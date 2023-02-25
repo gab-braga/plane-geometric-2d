28fc28e4e166c56493b5dcb638776b40a35cb0b2
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-02-25 12:47:51 -0300

    Circle and Pentagon

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -10,6 +10,39 @@ class Ponto {
     }
 }
 
+const PENTAGON = [
+    new Ponto(15, 9),
+    new Ponto(14, 10),
+    new Ponto(16, 10),
+    new Ponto(13, 11),
+    new Ponto(17, 11),
+    new Ponto(12, 12),
+    new Ponto(18, 12),
+    new Ponto(11, 13),
+    new Ponto(19, 13),
+    new Ponto(10, 14),
+    new Ponto(20, 14),
+    new Ponto(10, 15),
+    new Ponto(20, 15),
+    new Ponto(10, 16),
+    new Ponto(20, 16),
+    new Ponto(10, 17),
+    new Ponto(20, 17),
+    new Ponto(10, 18),
+    new Ponto(20, 18),
+    new Ponto(10, 19),
+    new Ponto(11, 19),
+    new Ponto(12, 19),
+    new Ponto(13, 19),
+    new Ponto(14, 19),
+    new Ponto(15, 19),
+    new Ponto(16, 19),
+    new Ponto(17, 19),
+    new Ponto(18, 19),
+    new Ponto(19, 19),
+    new Ponto(20, 19),
+];
+
 const SQUARE = [
     new Ponto(13, 13),
     new Ponto(13, 14),
@@ -78,13 +111,53 @@ const LOZENGE = [
     new Ponto(15, 14),
     new Ponto(16, 13),
     new Ponto(17, 12),
-]
+];
+
+const CIRCLE = [
+    new Ponto(14, 9),
+    new Ponto(15, 9),
+    new Ponto(16, 9),
+    new Ponto(17, 9),
+    new Ponto(12, 10),
+    new Ponto(13, 10),
+    new Ponto(18, 10),
+    new Ponto(19, 10),
+    new Ponto(11, 11),
+    new Ponto(20, 11),
+    new Ponto(10, 12),
+    new Ponto(21, 12),
+    new Ponto(10, 13),
+    new Ponto(21, 13),
+    new Ponto(9, 14),
+    new Ponto(22, 14),
+    new Ponto(9, 15),
+    new Ponto(22, 15),
+    new Ponto(9, 16),
+    new Ponto(22, 16),
+    new Ponto(9, 17),
+    new Ponto(22, 17),
+    new Ponto(10, 18),
+    new Ponto(21, 18),
+    new Ponto(10, 19),
+    new Ponto(21, 19),
+    new Ponto(11, 20),
+    new Ponto(20, 20),
+    new Ponto(12, 21),
+    new Ponto(13, 21),
+    new Ponto(18, 21),
+    new Ponto(19, 21),
+    new Ponto(14, 22),
+    new Ponto(15, 22),
+    new Ponto(16, 22),
+    new Ponto(17, 22)
+];
+
 const POINTS = [
     new Ponto(1, 3),
     new Ponto(1, 1),
     new Ponto(3, 1),
     new Ponto(3, 3)
-]
+];
 
 function criarGrade(length = LENGTH) {
     const table = document.getElementById("grade");
@@ -112,7 +185,9 @@ function desenharObjeto(pontos = []) {
             celula = table.children[y - 1].children[x - 1];
             celula.classList.add("active");
         }
-        catch(err) { }
+        catch (err) {
+            // Caso a coordenada não seja encontrada, o algoritmo ignora
+        }
     }
 }
 
@@ -137,13 +212,6 @@ function iniciarControles() {
 
 iniciarControles();
 
-function escalarGrade(e) {
-    const value = e.target.value;
-    LENGTH = value;
-    criarGrade();
-    desenharObjeto(OBJETO);
-}
-
 function selecionarObjeto(e) {
     let value = e.target.value;
     switch (value) {
@@ -156,6 +224,12 @@ function selecionarObjeto(e) {
         case "lozenge":
             desenharObjeto(LOZENGE);
             break;
+        case "pentagon":
+            desenharObjeto(PENTAGON);
+            break;
+        case "circle":
+            desenharObjeto(CIRCLE);
+            break;
         case "points":
             desenharObjeto(POINTS)
             break;
@@ -166,7 +240,7 @@ function criarNovoObjeto() {
     const coordinatControl = document.getElementById("coordinat-control");
     const value = coordinatControl.value;
     const cordenadas = value.trim().split(";").map(subtring => subtring.split(","));
-    const objeto = cordenadas.map(cordenada  => new Ponto(Number(cordenada[0]), Number(cordenada[1])));
+    const objeto = cordenadas.map(cordenada => new Ponto(Number(cordenada[0]), Number(cordenada[1])));
     desenharObjeto(objeto);
 }
 
@@ -176,16 +250,16 @@ function transladarObjeto() {
         x = Number(document.getElementById("translate-control-x").value);
         y = Number(document.getElementById("translate-control-y").value);
     }
-    catch(err) {
+    catch (err) {
         alert("Insira valores válidos de x e y.");
         return;
     }
-    if(OBJETO.length === 0) {
+    if (OBJETO.length === 0) {
         alert("Selecione ou crie um objeto.");
         return;
     }
     else {
-        for(let ponto of OBJETO) {
+        for (let ponto of OBJETO) {
             ponto.x += x;
             ponto.y += y;
         }
@@ -197,11 +271,11 @@ function rotacionarObjeto() {
     let angulo = 0;
     try {
         angulo = Number(document.getElementById("rotate-control").value);
-        if(angulo < 0 || angulo > 360) {
+        if (angulo < 0 || angulo > 360) {
             throw new Error();
         }
     }
-    catch(err) {
+    catch (err) {
         alert("Insira valor válido do ângulo.");
         return;
     }
@@ -211,7 +285,7 @@ function rotacionarObjeto() {
         [Math.sin(radianos), Math.cos(radianos)]
     ];
     let objetoRotacionado = [];
-    for(let ponto of OBJETO) {
+    for (let ponto of OBJETO) {
         let x = Math.round((matrizRotacao[0][0] * ponto.x) + (matrizRotacao[0][1] * ponto.y));
         let y = Math.round((matrizRotacao[1][0] * ponto.x) + (matrizRotacao[1][1] * ponto.y));
         objetoRotacionado.push(new Ponto(x, y));
@@ -220,7 +294,7 @@ function rotacionarObjeto() {
     const centroideObjetoRotacionado = obterCentroide(objetoRotacionado);
     const direfencaX = centroideObjetoRotacionado.x - centroideObjeto.x;
     const direfencaY = centroideObjetoRotacionado.y - centroideObjeto.y;
-    for(let ponto of objetoRotacionado) {
+    for (let ponto of objetoRotacionado) {
         ponto.x -= direfencaX;
         ponto.y -= direfencaY;
     }
@@ -232,20 +306,20 @@ function escalonarObjeto() {
     try {
         scaleX = Number(document.getElementById("scalling-control-x").value);
         scaleY = Number(document.getElementById("scalling-control-y").value);
-        if(( scaleX && scaleY ) <= 0 ) 
-            throw new Error();    
+        if ((scaleX && scaleY) <= 0)
+            throw new Error();
     }
-    catch(err) {
+    catch (err) {
         alert("Insira valores válidos de x e y.");
         return;
     }
-    if(OBJETO.length === 0) {
+    if (OBJETO.length === 0) {
         alert("Selecione ou crie um objeto.");
         return;
     }
     else {
         let objetoEscalonado = [];
-        for(let ponto of OBJETO) {
+        for (let ponto of OBJETO) {
             let x = Math.round(ponto.x * scaleX);
             let y = Math.round(ponto.y * scaleY);
             objetoEscalonado.push(new Ponto(x, y));
@@ -254,27 +328,11 @@ function escalonarObjeto() {
         const centroideObjetoEscalonado = obterCentroide(objetoEscalonado);
         const direfencaX = centroideObjetoEscalonado.x - centroideObjeto.x;
         const direfencaY = centroideObjetoEscalonado.y - centroideObjeto.y;
-        for(let ponto of objetoEscalonado) {
+        for (let ponto of objetoEscalonado) {
             ponto.x -= direfencaX;
             ponto.y -= direfencaY;
         }
-       desenharObjeto(objetoEscalonado);
-    }
-}
-
-function reiniciarGrade() {
-    OBJETO = [];
-    criarGrade()
-}
-
-function removerBordaGrade(e) {
-    const isCheck = e.target.checked;
-    const table = document.getElementById("grade");
-    if(isCheck) {
-        table.classList.remove("not-grade");
-    }
-    else {
-        table.classList.add("not-grade");
+        desenharObjeto(objetoEscalonado);
     }
 }
 
@@ -283,8 +341,31 @@ function obterCentroide(objeto = OBJETO) {
     let maxX = Math.max(...objeto.map(ponto => ponto.x));
     let minY = Math.min(...objeto.map(ponto => ponto.y));
     let maxY = Math.max(...objeto.map(ponto => ponto.y));
-    let x = Math.round((minX+maxX)/2);
-    let y = Math.round((minY+maxY)/2);
+    let x = Math.round((minX + maxX) / 2);
+    let y = Math.round((minY + maxY) / 2);
     let centroide = new Ponto(x, y);
     return centroide;
+}
+
+function reiniciarGrade() {
+    OBJETO = [];
+    criarGrade()
+}
+
+function escalarGrade(e) {
+    const value = e.target.value;
+    LENGTH = value;
+    criarGrade();
+    desenharObjeto(OBJETO);
+}
+
+function removerBordaGrade(e) {
+    const isCheck = e.target.checked;
+    const table = document.getElementById("grade");
+    if (isCheck) {
+        table.classList.remove("not-grade");
+    }
+    else {
+        table.classList.add("not-grade");
+    }
 }
